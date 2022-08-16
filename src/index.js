65d3c95d42ac7ad60d4f312ff333aa1ae49edaa3
docbull <fudghks21@gmail.com>
2022-08-16 23:49:28 +0900

    js-libp2p ping pong example

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createLibp2p } from "libp2p"
 import { TCP } from "@libp2p/tcp"
 import { Noise } from '@chainsafe/libp2p-noise'
 import { Mplex } from '@libp2p/mplex'
+import { multiaddr } from 'multiaddr'
 
 const main = async () => {
     const node = await createLibp2p({
@@ -22,8 +23,23 @@ const main = async () => {
         console.log(addr.toString())
     })
     
-    await node.stop()
-    console.log("libp2p has stopped");    
+    if (process.argv.length >= 3) {
+        const ma = multiaddr(process.argv[2])
+        console.log(`pinging remote peer at ${process.argv[2]}`)
+        const latency = await node.ping(ma)
+        console.log(`pinged ${process.argv[2]} in ${latency}ms`)
+    } else {
+        console.log('no remote peer address given, skipping ping')
+    }
+
+    const stop = async () => {
+        await node.stop()
+        console.log('libp2p has stopped')
+        process.exit(0)
+    }
+
+    process.on('SIGTERM', stop)
+    process.on('SIGINT', stop)
 }
 
 main().then().catch(console.error)
